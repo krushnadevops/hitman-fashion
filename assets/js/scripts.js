5d94e34af3be8be9bebab84e9d63415bd6feef0b
krushnadevops <137031288+krushnadevops@users.noreply.github.com>
2025-06-16 12:13:51 +0530

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+// scripts.js - Slider Functionality for Homepage
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) {
+      slide.classList.add("active");
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Initial display
+showSlide(currentSlide);
+
+// Auto-change slide every 4 seconds
+setInterval(nextSlide, 4000);
